36b8f47e2f72588097e9c9b4d92753b149813fec
YuanzhanHuo <41846907+YuanzhanHuo@users.noreply.github.com>
2018-08-24 05:00:59 +1000

    Add project files.

---
+++ uncrowded/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//Navbar is tranparent on top and setted bg-color when scoll down
+(function ($) {
+
+    var navbar = $('.navbar');
+    var lastScrollTop = 0;
+
+    $(window).scroll(function () {
+        var st = $(this).scrollTop();
+        // Scroll down
+        if (st > lastScrollTop) {
+            navbar.fadeOut();
+        }
+        // Scroll up but still lower than 200 (change that to whatever suits your need)
+        else if (st < lastScrollTop && st > 100) {
+            navbar.fadeIn();
+            navbar.removeClass('navbar-light bg-transparent').addClass('navbar-dark bg-custom');
+        }
+        // Reached top
+        else {
+            navbar.removeClass('navbar-dark bg-custom').addClass('navbar-light bg-transparent');
+        }
+        lastScrollTop = st;
+    });
+
+})(jQuery);
